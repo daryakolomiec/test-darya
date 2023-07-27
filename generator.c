cbf76e298183577743d241bb810fa2d5609fb022
Darya <122486804+daryakolomiec@users.noreply.github.com>
2023-07-27 11:26:06 +0300

    Create generator.c

---
+++ generator.c
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+std::string generateCode() {
+    /**
+     * This function generates a random code and returns it as a string.
+     * 
+     * Returns:
+     * std::string: The generated code
+     */
+    
+    // Generate a random code
+    std::string code = "ABC123";
+    
+    return code;
+}
+
+int main() {
+    // Generate a code
+    std::string generatedCode = generateCode();
+    
+    // Print the generated code
+    std::cout << "Generated Code: " << generatedCode << std::endl;
+    
+    return 0;
+}
